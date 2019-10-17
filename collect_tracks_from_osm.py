9210453e0714239bdb34a73331f9dafefccf373a
Noémie <noemie.lehuby@zaclys.net>
2019-10-17 10:07:36 +0200

    fix collect all tracks

--- collect_tracks_from_osm.py
+++ collect_tracks_from_osm.py
@@ -3,6 +3,8 @@
 
 import requests
 import xml.dom.minidom as dom
+from lxml import html
+
 
 def dl_track(track_url, track_name, destination="."):
     #TODO : skip download if the file already exist ?
@@ -32,9 +34,38 @@ def download_last_tracks_from_tag(tag, destination="."):
     url1 = "https://www.openstreetmap.org/traces/tag/"+tag+"/rss"
     download_from_rss(url1, destination)
 
+def collect_tracks_from_one_page(page_prefix, page_id):
+    page_url = "{}/page/{}".format(page_prefix, page_id) #"https://www.openstreetmap.org/traces/tag/abidjantransport/page/4"
+    page = requests.get(page_url)
+    tree = html.fromstring(page.content)
+
+    trace_names = tree.xpath('//tr/td[2]/a[1]/text()')
+    trace_links = tree.xpath('//tr/td[2]/a[1]/attribute::href')
+
+    results = []
+    for id_, trace_name in enumerate(trace_names):
+        elem = {}
+        elem['name'] = trace_name
+        elem['user'] = trace_links[id_].split('/')[2]
+        elem['trace_id'] = trace_links[id_].split('/')[-1]
+        elem['track_url'] = "http://www.openstreetmap.org/trace/{}/data".format(elem['trace_id'])
+        results.append(elem)
+    return results
+
+def collect_all_tracks_from_tag(tag):
+    elems = []
+    for page_number in range(124):
+        result_page = collect_tracks_from_one_page("https://www.openstreetmap.org/traces/tag/{}".format(tag), page_number +1)
+        elems += result_page
+        if not result_page :
+            break
+    return elems
 
 def download_all_tracks_from_tag(tag, destination="."):
-    pass
+    traces_info = collect_all_tracks_from_tag(tag)
+    for elem in traces_info :
+        dl_track(elem['track_url'], elem['name'], destination)
+    return traces_info
 
 if __name__ == '__main__':
     for index in range(10):
